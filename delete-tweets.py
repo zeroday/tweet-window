08ddadacb196a202b8136317a4c35f9b5457a622
# <#>
2017-12-25 14:30:35 -0500

    Access Twitter credentials from environment.

---
+++ delete-tweets.py
@@ -0,0 +1,9 @@
+import tweepy
+import csv
+import os
+
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+access_key = os.environ['TWITTER_ACCESS_KEY']
+access_secret = os.environ['TWITTER_ACCESS_SECRET']
+
