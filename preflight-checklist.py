073383c2842161baf688367f125a7f0fc8a03ff1
# <#>
2017-12-30 20:44:13 -0500

    Test script to check for successful authentication.

---
+++ preflight-checklist.py
@@ -0,0 +1,27 @@
+import tweepy
+import csv
+import os
+import json
+import datetime
+import time
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_KEY = os.environ['TWITTER_ACCESS_KEY']
+ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+# copied from https://pushpullfork.com/i-deleted-tweets/
+def oauth_login(consumer_key, consumer_secret):
+    """Authenticate with twitter using OAuth"""
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth_url = auth.get_authorization_url()
+    verify_code = raw_input("Authenticate at %s and then enter you verification code here > " % auth_url)
+    auth.get_access_token(verify_code)
+    return tweepy.API(auth)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+me = api.me()
+print("Authenticated as: %s" % me.screen_name)
+
